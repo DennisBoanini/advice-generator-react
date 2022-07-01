dacfc156323581b85682dfeabec4b69ba6496c79
Dennis A. Boanini <dennis.boanini01@gmail.com>
2022-07-02 01:01:58 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [advice, setAdvice] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.adviceslip.com/advice')
+            .then(response => response.json())
+            .then(data => setAdvice(data.slip.advice));
+    }, [])
+
+    const getAdvice = () => {
+        fetch('https://api.adviceslip.com/advice')
+            .then(response => response.json())
+            .then(data => setAdvice(data.slip.advice));
+    }
+
+    return (
+        <div className="App">
+            <section className="advice-container">
+                <p className="advice-number">advice #<span id="advice-id"></span></p>
+                <p className="advice-text" id="advice-text">
+                    {advice}
+                </p>
+                <div className="separator"></div>
+                <button className="advice-generator" id="advice-generator-btn" onClick={getAdvice} />
+            </section>
+        </div>
+    );
 }
 
 export default App;
